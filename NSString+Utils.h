bc4298e2743f43d2a08a0df594632130eccb4ec9
Pablo A. Pérez-Martínez <pablo.alejandro.pm@gmail.com>
2016-02-19 09:55:25 +0000

    Methods replaced
    Methods replaced by a more specific that allows multiple
    NSAttributedStrings

--- NSString+Utils.h
+++ NSString+Utils.h
@@ -3,7 +3,6 @@
 //  Pablo A. Pérez-Martínez
 //
 //  Created by Pau on 02/07/2014.
-//  Copyright (c) 2014 Pablo A. Pérez-Martínez. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -12,28 +11,11 @@
 @interface NSString(Utils)
 
 /**
- * Makes a part of a string smaller
- * @param string Full string
- * @param substring Substring to change the font
+ * All the arrays must contain at least one object. If we want to use the same colour or font
+ * for all the substrings, then we can just add one object to "fonts" or "colours", first object
+ * in the arrays will be the default value.
  */
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string substring:(NSString *)substring;
 
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string substring:(NSString *)substring substringSize:(CGFloat)substringSize;
-
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string substring:(NSString *)substring substringFont:(UIFont *)substringFont;
-
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string stringFont:(UIFont *)stringFont substring:(NSString *)substring substringFont:(UIFont *)substringFont;
-
-+ (CGFloat)heightForText:(NSString *)text labelWidth:(float)labelWidth font:(UIFont *)font;
-
-- (NSAttributedString *)attributedStringWithKeywords:(NSArray *)keywords fontName:(NSString *)fontName fontSize:(CGFloat)fontSize;
-
-- (NSAttributedString *)attributedStringWithKeywords:(NSArray *)keywords font:(UIFont *)font;
-
-- (NSAttributedString *)attributedStringWithKeywords:(NSArray *)keywords font:(UIFont *)font color:(UIColor *)color;
-
-+ (NSAttributedString *)textAsLink:(NSDictionary *)keysAndUrls completeText:(NSString *)completeText;
-
-+ (NSAttributedString *)textAsLink:(NSDictionary *)keysAndUrls completeText:(NSString *)completeText preventPrefixes:(NSArray *)prefixes;
++ (NSMutableAttributedString *)attributedStringWithStrings:(NSArray <NSString *> *)strings fonts:(NSArray <UIFont *> *)fonts colours:(NSArray <UIColor *> *)colours;
 
 @end
--- NSString+Utils.m
+++ NSString+Utils.m
@@ -3,242 +3,31 @@
 //  Pablo A. Pérez-Martínez
 //
 //  Created by Pau on 02/07/2014.
-//  Copyright (c) 2014 Pablo A. Pérez-Martínez. All rights reserved.
 //
 
 #import "NSString+Utils.h"
 
 @implementation NSString(Utils)
 
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string substring:(NSString *)substring
++ (NSMutableAttributedString *)attributedStringWithStrings:(NSArray <NSString *> *)strings fonts:(NSArray <UIFont *> *)fonts colours:(NSArray <UIColor *> *)colours
 {
-    if(!string)
-        return nil;
+    NSAssert(fonts.count > 0 && colours.count > 0, @"Attributes arrays must contain at least one default object");
     
-    UIFont *font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:27.0];
-    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:string];
+    NSMutableAttributedString *str = [NSMutableAttributedString new];
     
-    if([substring length] < 1)
-        return str;
-    
-    @try {
-        [str addAttribute:NSFontAttributeName value:font range:[string rangeOfString:substring]];
-    }
-    @catch (NSException *exception) {
-        NSLog(@"%s: Exception -> %@", __func__, exception);
-    }
-    @finally {
-        return str;
-    }
-}
-
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string substring:(NSString *)substring substringSize:(CGFloat)substringSize
-{
-    if(!string)
-        return nil;
-    
-    UIFont *font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:substringSize];
-    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:string];
-    
-    if([substring length] < 1)
-        return str;
-    
-    @try {
-        [str addAttribute:NSFontAttributeName value:font range:[string rangeOfString:substring]];
-    }
-    @catch (NSException *exception) {
-        NSLog(@"%s: Exception -> %@", __func__, exception);
-    }
-    @finally {
-        return str;
-    }
-}
-
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string substring:(NSString *)substring substringFont:(UIFont *)substringFont
-{
-    if(!string)
-        return nil;
-    
-    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:string];
-    
-    if([substring length] < 1)
-        return str;
-    
-    @try {
-        [str addAttribute:NSFontAttributeName value:substringFont range:[string rangeOfString:substring]];
-    }
-    @catch (NSException *exception) {
-        NSLog(@"%s: Exception -> %@", __func__, exception);
-    }
-    @finally {
-        return str;
-    }
-}
-
-+ (NSMutableAttributedString *)attributedStringWithString:(NSString *)string stringFont:(UIFont *)stringFont substring:(NSString *)substring substringFont:(UIFont *)substringFont
-{
-    if(!string) {
-        return nil;
-    }
-    
-    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:string];
-    
-    if([string rangeOfString:string].location == NSNotFound) {
-        return nil;
-    }
-    
-    NSRange range = [string rangeOfString:string];
-    
-    if(str)
-        [str addAttribute:NSFontAttributeName value:stringFont range:range];
-    
-    if([substring length] < 1)
-        return str;
-
-    @try {
-        [str addAttribute:NSFontAttributeName value:substringFont range:[string rangeOfString:substring]];
-    }
-    @catch (NSException *exception) {
-        NSLog(@"%s: Exception -> %@", __func__, exception);
-    }
-    @finally {
-        return str;
-    }
-}
-
-+ (CGFloat)heightForText:(NSString *)text labelWidth:(float)labelWidth font:(UIFont *)font
-{
-    if(!text || labelWidth <= 0 || !font)
-        return 0.0f;
-    
-    NSDictionary *attributes = @{NSFontAttributeName:font};
-    CGSize size = CGSizeMake(labelWidth, 9999);
-    
-    CGRect rect = [text boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];
-    CGSize textSize = rect.size;
-    
-    return textSize.height;
-}
-
-- (NSAttributedString *)attributedStringWithKeywords:(NSArray *)keywords fontName:(NSString *)fontName fontSize:(CGFloat)fontSize
-{
-    NSMutableAttributedString * string = [[NSMutableAttributedString alloc] initWithString:self];
-    
-    UIFont *font = [UIFont fontWithName:[NSString stringWithFormat:@"%@-bold", fontName] size:fontSize];
-    
-    for (NSString * word in keywords) {
+    for(NSString *string in strings) {
         
-        NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:word options:NSRegularExpressionCaseInsensitive error:nil];
+        NSInteger index = [strings indexOfObject:string];
+        UIFont *font = fonts.count > index ? [fonts objectAtIndex:index] : [fonts firstObject];
+        UIColor *colour = colours.count > index ? [colours objectAtIndex:index] : [colours firstObject];
+        NSDictionary *attributes = @{NSFontAttributeName : font,
+                                     NSForegroundColorAttributeName : colour};
+        NSAttributedString *attrStr = [[NSAttributedString alloc] initWithString:string attributes:attributes];
         
-        //  enumerate matches
-        NSRange range = NSMakeRange(0, [self length]);
-        [expression enumerateMatchesInString:self options:0 range:range usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
-            
-            NSRange newRange = [result rangeAtIndex:0];
-            
-            @try {
-                [string addAttribute: NSFontAttributeName value:font range: newRange];
-            }
-            @catch (NSException *exception) {
-                NSLog(@"%s %@", __func__, exception);
-            }
-        }];
-    }
-
-    return string;
-}
-
-- (NSAttributedString *)attributedStringWithKeywords:(NSArray *)keywords font:(UIFont *)font
-{
-    NSMutableAttributedString * string = [[NSMutableAttributedString alloc] initWithString:self];
-    
-    for (NSString * word in keywords) {
-        
-        NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:word options:NSRegularExpressionCaseInsensitive error:nil];
-        
-        //  enumerate matches
-        NSRange range = NSMakeRange(0, [self length]);
-        [expression enumerateMatchesInString:self options:0 range:range usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
-            
-            NSRange newRange = [result rangeAtIndex:0];
-            
-            @try {
-                [string addAttribute: NSFontAttributeName value:font range: newRange];
-            }
-            @catch (NSException *exception) {
-                NSLog(@"%s %@", __func__, exception);
-            }
-        }];
+        [str appendAttributedString:attrStr];
     }
     
-    return string;
-}
-
-- (NSAttributedString *)attributedStringWithKeywords:(NSArray *)keywords font:(UIFont *)font color:(UIColor *)color
-{
-    NSMutableAttributedString * string = [[NSMutableAttributedString alloc] initWithString:self];
-    
-    for (NSString * word in keywords) {
-        
-        NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:word options:NSRegularExpressionCaseInsensitive error:nil];
-        
-        //  enumerate matches
-        NSRange range = NSMakeRange(0, [self length]);
-        [expression enumerateMatchesInString:self options:0 range:range usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
-            
-            NSRange newRange = [result rangeAtIndex:0];
-            
-            @try {
-                [string addAttribute: NSFontAttributeName value:font range: newRange];
-                
-                [string addAttribute: NSForegroundColorAttributeName value:color range: newRange];
-            }
-            @catch (NSException *exception) {
-                NSLog(@"%s %@", __func__, exception);
-            }
-        }];
-    }
-    
-    return string;
-}
-
-+ (NSAttributedString *)textAsLink:(NSDictionary *)keysAndUrls completeText:(NSString *)completeText
-{
-    NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:completeText];
-    
-    for(NSString *key in [keysAndUrls allKeys]) {
-        
-        NSRange range = [completeText rangeOfString:key options:NSCaseInsensitiveSearch];
-        
-        if (range.location != NSNotFound) {
-            
-            [attr addAttribute:NSLinkAttributeName value:[keysAndUrls objectForKey:key] range:range];
-            [attr addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:range];
-        }
-    }
-    
-    return attr;
-}
-
-+ (NSAttributedString *)textAsLink:(NSDictionary *)keysAndUrls completeText:(NSString *)completeText preventPrefixes:(NSArray *)prefixes
-{
-    NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:completeText];
-    
-    for(NSString *key in [keysAndUrls allKeys]) {
-        
-        NSRange range = [completeText rangeOfString:key options:NSCaseInsensitiveSearch];
-        
-        if (range.location != NSNotFound) {
-            
-            if(![prefixes containsObject:[completeText substringWithRange:NSMakeRange(MAX(range.location - 1, 0), 1)]]) {
-                
-                [attr addAttribute:NSLinkAttributeName value:[keysAndUrls objectForKey:key] range:range];
-                [attr addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:range];
-            }
-        }
-    }
-    
-    return attr;
+    return str;
 }
 
 @end
